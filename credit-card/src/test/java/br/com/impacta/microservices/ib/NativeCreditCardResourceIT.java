5527f15054ff407f02d00b12eb64c3c3d04b8e2a
Paulo Marques <pauloedums@gmail.com>
2021-08-27 19:46:54 -0300

    Adicionando os projetos de Cartão de Crédito e Investimentos

---
+++ credit-card/src/test/java/br/com/impacta/microservices/ib/NativeCreditCardResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.impacta.microservices.ib;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCreditCardResourceIT extends CreditCardResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ investments/src/test/java/br/com/impacta/microservices/ib/NativeInvestmentsResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.impacta.microservices.ib;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeInvestmentsResourceIT extends InvestmentsResourceTest {
+
+    // Execute the same tests but in native mode.
+}

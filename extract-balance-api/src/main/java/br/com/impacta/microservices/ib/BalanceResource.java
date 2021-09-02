4c48804dcb8a2a4d64ae41edce6a0e59c12816e3
Paulo Marques <pauloedums@gmail.com>
2021-09-02 00:19:56 -0300

    Ajustes nos serviços de saldo-extrato para adição de circuit break

--- extract-balance-api/src/main/java/br/com/impacta/microservices/ib/BalanceResource.java
+++ extract-balance-api/src/main/java/br/com/impacta/microservices/ib/BalanceResource.java
@@ -8,6 +8,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
 import br.com.impacta.microservices.ib.model.Balance;
 import br.com.impacta.microservices.ib.services.ExtractBalanceService;
 
@@ -18,10 +23,24 @@ public class BalanceResource {
     ExtractBalanceService extractService;
 
     @GET
+
+    @Fallback(fallbackMethod = "fallbackGetBalance")
+    @Timeout(2000)
+    @CircuitBreaker(
+        requestVolumeThreshold=4,
+        failureRatio=0.5,
+        successThreshold=2
+    )
+    @Retry(maxRetries = 5)
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBalance(){
         Balance balance = extractService.getBalance();
         return Response.ok(balance).build();
+    }
+
+
+    private Response fallbackGetBalance(){
+        return Response.ok("Não foi possível encontrar dados para criar o saldo.").build();
     }
 }

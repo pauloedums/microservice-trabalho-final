7005860e15cfb58ba96915ef9056f3263ae30281
Paulo Marques <pauloedums@gmail.com>
2021-09-21 20:30:25 -0300

    Delete docker-images.sh

--- docker-images.sh
+++
@@ -1,6 +0,0 @@
-cd ./credit && ./mvnw package
-cd ./../debit && ./mvnw package
-cd ./../extract-balance-api && ./mvnw package
-cd ./../credit-card && ./mvnw package
-cd ./../investments && ./mvnw package
-cd ./../account && ./mvnw package
